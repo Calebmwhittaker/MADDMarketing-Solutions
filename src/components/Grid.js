09f1abf535ce0a4d938e7ca2e61db7f5187e84a4
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2022-11-12 03:42:14 -0500

    Responsive services icons

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,20 +4,20 @@ export default function Grid() {
   return (
     <div className='services'>
         <div className="row">
-  <div className="col">
+  <div className="col col-lg-6 col-xl-3">
   <img src="computer_image.png" alt="Website Design"></img>
             <p>Website Design</p>
   </div>
-  <div className="col">
+  <div className="col col-lg-6 col-xl-3">
   <img src="social_media_image.png" alt="Social Media"></img>
             <p>Social Media</p>
   </div>
-  <div className="w-100"></div>
-  <div className="col">
+  {/* <div className="w-100"></div> */}
+  <div className="col col-lg-6 col-xl-3">
   <img src="lightbulb_image.png" alt="Strategy"></img>
             <p>Strategy</p>
   </div>
-  <div className="col">
+  <div className="col col-lg-6 col-xl-3">
   <img src="coaching_image.png" alt="Coaching"></img>
             <p>Coaching</p>
   </div>
