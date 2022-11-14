a18dadc677c2b484a33d37ff55542ec25dbf9ee1
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2022-11-13 19:05:49 -0500

    Centered Home Page

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Grid() {
   return (
     <div className='services'>
-        <div className="row col-12">
+        <div className="row col-12 mr-2">
   <div className="col col-lg-6 col-xl-3">
   <img className="my-4" width="300px" src="computer_image.png" alt="Website Design"></img>
             <p>Website Design</p>
@@ -20,6 +20,17 @@ export default function Grid() {
   <img className="my-4" width="300px" src="coaching_image.png" alt="Coaching"></img>
             <p>Coaching</p>
   </div>
+  <div className="learnMore col-12 py-3">
+        <button className='btn btn-dark'>
+            <a 
+              href="https://www.maddmarketingsolutions.com/services"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Our Services
+            </a>
+          </button>
+          </div>
 </div>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="main-title">
         <h1>MaddMarketing Solutions</h1>
       </div>
-      <Carousel />
+      {/* <Carousel /> */}
       <main>
         <div className="home-approach">
         <h2>Our approach</h2>
@@ -23,22 +23,11 @@ const Home = () => {
         <div className="services-container">
         <h2 className='pt-4 text-light'>Our services</h2>
         <Grid />
-        <div className="learnMore">
-        <button>
-            <a 
-              href="https://www.maddmarketingsolutions.com/services"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Our Services
-            </a>
-          </button>
-          </div>
         </div>
         <Testimonials />
         <div className="contact-container">
           <h2>Get started with MaddMarketing today!</h2>
-          <button className='btn'><a href="https://www.maddmarketingsolutions.com/contact" target="_blank" rel="noreferrer">Contact Us</a></button>
+          <button className='btn btn-dark'><a href="https://www.maddmarketingsolutions.com/contact" target="_blank" rel="noreferrer">Contact Us</a></button>
         </div>
       </main>
       <Footer />
