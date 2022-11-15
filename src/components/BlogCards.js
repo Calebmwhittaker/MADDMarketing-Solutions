40d3106800fcc714f79b93a44342ab8a5b938cd6
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2022-11-14 19:42:48 -0500

    Centered Blog Page

--- src/components/BlogCards.js
+++ src/components/BlogCards.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 export default function BlogCards() {
   return (
-    <div>
-        <div className="row col-12">
-            <div className="col-12 col-lg-6 text-center px-3">
-                <img className="my-4" src="blog-photo-1.jpg" alt="Website Design"></img>
+    <div className='blog'>
+        <div className="row">
+            <div className="col col-lg-6 col-xl-4 text-center">
+                <img className="my-4" src="blog-photo-1.jpg" width="400px" alt="Website Design"></img>
                 <p>Digital Marketing 8/11/22</p>
                 <h1>Why you need a digital marketer on your team</h1>
             </div>
-            <div className="col-12 col-lg-6 text-center px-3">
-                <img className="my-4" src="blog-photo-2.jpg" alt="Social Media"></img>
+            <div className="col col-lg-6 col-xl-4 text-center">
+                <img className="my-4" src="blog-photo-2.jpg" width="400px" alt="Social Media"></img>
                 <p>Digital Marketing 7/27/22</p>
                 <h1>Become a storyteller! Use video to grow your audience.</h1>
             </div>
-            <div className="col-12 text-center">
-                <img className="my-4" src="blog-photo-3.jpg" alt="Social Media"></img>
+            <div className="col col-lg-6 col-xl-4 text-center">
+                <img className="my-4" src="blog-photo-3.jpg" width="400px" alt="Social Media"></img>
                 <p>Digital Marketing 7/15/22</p>
                 <h1>What is Digital Marketing? How to use online tools for customer growth.</h1>
             </div>
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,35 +3,35 @@ import React from 'react'
 export default function Grid() {
   return (
     <div className='services'>
-        <div className="row col-12 mr-2">
-  <div className="col col-lg-6 col-xl-3">
-  <img className="my-4" width="300px" src="computer_image.png" alt="Website Design"></img>
+        <div className="row">
+          <div className="col col-lg-6 col-xl-3">
+            <img className="my-4" width="300px" src="computer_image.png" alt="Website Design"></img>
             <p>Website Design</p>
-  </div>
-  <div className="col col-lg-6 col-xl-3">
-  <img className="my-4" width="300px" src="social_media_image.png" alt="Social Media"></img>
-            <p>Social Media</p>
-  </div>
-  <div className="col col-lg-6 col-xl-3">
-  <img className="my-4" width="300px" src="lightbulb_image.png" alt="Strategy"></img>
-            <p>Strategy</p>
-  </div>
-  <div className="col col-lg-6 col-xl-3">
-  <img className="my-4" width="300px" src="coaching_image.png" alt="Coaching"></img>
-            <p>Coaching</p>
-  </div>
-  <div className="learnMore col-12 py-3">
-        <button className='btn btn-dark'>
-            <a 
-              href="https://www.maddmarketingsolutions.com/services"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Our Services
-            </a>
-          </button>
           </div>
-</div>
+          <div className="col col-lg-6 col-xl-3">
+            <img className="my-4" width="300px" src="social_media_image.png" alt="Social Media"></img>
+            <p>Social Media</p>
+          </div>
+          <div className="col col-lg-6 col-xl-3">
+            <img className="my-4" width="300px" src="lightbulb_image.png" alt="Strategy"></img>
+            <p>Strategy</p>
+          </div>
+          <div className="col col-lg-6 col-xl-3">
+            <img className="my-4" width="300px" src="coaching_image.png" alt="Coaching"></img>
+            <p>Coaching</p>
+          </div>
+          <div className="learnMore col-12 py-3">
+            <button className='btn btn-dark'>
+              <a 
+                href="https://www.maddmarketingsolutions.com/services"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Our Services
+              </a>
+            </button>
+          </div>
+        </div>
     </div>
   )
 }
